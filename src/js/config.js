e31c3eb2ee1d9dd05c9a76a10b0c59bf64f71644
NodarBakradze <realnodarbakradze@gmail.com>
2023-01-30 12:33:49 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = `49160473-8a3d-46d9-9d85-8db0e07d2de2`;
+export const MODAL_CLOSE_SEC = 2.5;
